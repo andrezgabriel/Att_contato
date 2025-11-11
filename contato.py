47677c95797e55f6b5000f768941c6a1b3158501
ANDREZ GABRIEL CAVALCANTI CORNELLI <105998428+andrezgabriel@users.noreply.github.com>
2025-11-11 19:06:58 -0300

    Add files via upload

---
+++ contato.py
@@ -0,0 +1,12 @@
+contato = {}
+
+contato["nome"] = input("Digite o nome do contato: ")
+contato["telefone"] = input("Digite o número de telefone: ")
+contato["email"] = input("Digite o endereço de email: ")
+
+print("\n--- Informações do Contato ---")
+print(f"Nome: {contato['nome']}")
+print(f"Telefone: {contato['telefone']}")
+print(f"Email: {contato['email']}")
+
+
